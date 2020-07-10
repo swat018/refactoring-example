c5485dd832f7afa9ce2be30b46a715946fd59186
jinwoo park <swat018@gmail.com>
2020-07-11 03:52:01 +0900

    리플랙션 API - 클래스 정보 수정 또는 실행

--- src/main/java/com/swat018/App.java
+++ src/main/java/com/swat018/App.java
@@ -2,7 +2,7 @@ package com.swat018;
 
 import org.w3c.dom.ls.LSOutput;
 
-import java.lang.reflect.Modifier;
+import java.lang.reflect.*;
 import java.util.Arrays;
 
 /**
@@ -11,7 +11,7 @@ import java.util.Arrays;
  */
 public class App 
 {
-    public static void main( String[] args ) throws ClassNotFoundException {
+    public static void main( String[] args ) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException {
 /*        Class<Book> bookClass = Book.class;
         Book book = new Book();
 
@@ -48,9 +48,11 @@ public class App
         Arrays.stream(Book.class.getMethods()).forEach(m-> {
             int modifiers = m.getModifiers();
         });*/
+
 //        Arrays.stream(MyBook.class.getAnnotations()).forEach(System.out::println);
 //        Arrays.stream(MyBook.class.getDeclaredAnnotations()).forEach(System.out::println);
-        Arrays.stream(Book.class.getDeclaredFields()).forEach(f->{
+
+/*        Arrays.stream(Book.class.getDeclaredFields()).forEach(f->{
             Arrays.stream(f.getAnnotations()).forEach(a-> {
                 if(a instanceof MyAnnotation) {
                     MyAnnotation myAnnotation = (MyAnnotation) a;
@@ -58,6 +60,29 @@ public class App
                     System.out.println(myAnnotation.number());
                 }
             });
-        });
+        });*/
+
+        Class<?> bookClass = Class.forName("com.swat018.Book");
+        Constructor<?> constructor = bookClass.getConstructor(String.class);
+        Book book = (Book) constructor.newInstance("MyBook");
+        System.out.println(book);
+
+        Field a = Book.class.getDeclaredField("A");
+        System.out.println(a.get(null));
+        a.set(null, "AAAAAAA");
+        System.out.println(a.get(null));
+
+        Field b = Book.class.getDeclaredField("B");
+        b.setAccessible(true);
+        System.out.println(b.get(book));
+        b.set(book, "BBBBBBB");
+        System.out.println(b.get(book));
+
+        Method c = Book.class.getDeclaredMethod("c");
+        c.setAccessible(true);
+
+        Method d = Book.class.getDeclaredMethod("sum", int.class, int.class);
+        int invoke = (int) d.invoke(book, 1,2);
+        System.out.println(invoke);
     }
 }
--- src/main/java/com/swat018/Book.java
+++ src/main/java/com/swat018/Book.java
@@ -3,40 +3,25 @@ package com.swat018;
 import java.lang.annotation.Inherited;
 import java.util.function.DoubleToIntFunction;
 
-@MyAnnotation(value = "seojin")
 public class Book {
 
-    @MyAnnotation
-    private String a = "a";
+    public static String A = "A";
 
-    private static String B = "BOOK";
+    private String B = "B";
 
-    private static final String C = "BOOK";
+    private void c() {
+        System.out.println("C");
+    }
 
-    public String d = "d";
-
-    protected String e = "e";
+    public int sum(int left, int right) {
+        return left + right;
+    }
 
     public Book() {
 
     }
-    @AnotherAnnotation
-    public Book(String a, String d, String e) {
-        this.a = a;
-        this.d = d;
-        this.e = e;
-    }
 
-    private void f() {
-        System.out.println("F");
-    }
-
-    @AnotherAnnotation
-    public void g() {
-        System.out.println("g");
-    }
-
-    public int h() {
-        return 100;
+    public Book(String b) {
+        B = b;
     }
 }
